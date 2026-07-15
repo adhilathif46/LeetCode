572551f4eb479245ece1e26b276a37f41defc3f9
Adhil Athif M <adhilathif46@gmail.com>
2026-07-15 11:22:35 +0530

    Time: 0 ms (100%), Space: 43 MB (52.47%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        String s1 = s.trim();
+        for(int i = s1.length()-1 ; i >= 0 ; i--){
+            if(s1.charAt(i)!=' '){
+                length++;
+            }
+            else{
+                break;
+            }
+        }
+        return length;
+    }
+}
