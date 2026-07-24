1f32f93558c63c2311c6169e93a3272a135f8da7
Adhil Athif M <adhilathif46@gmail.com>
2026-07-24 09:20:15 +0530

    Time: 7 ms (28.94%), Space: 99.4 MB (22.8%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,28 +10,18 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
-            return true;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(ListNode curr = head; curr!=null ; curr = curr.next){
+            list.add(curr.val);
         }
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode prev = null;
-        while(slow != null){
-            ListNode next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
-        while(prev != null){
-            if(head.val != prev.val){
+        int i = 0 ;
+        int j = list.size()-1;
+        while(i<j){
+            if(!list.get(i).equals(list.get(j))){
                 return false;
             }
-            head = head.next;
-            prev = prev.next;
+            i++;
+            j--;
         }
         return true;
 
