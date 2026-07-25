62f620fd8c434799bfcd3f098a1a4292bb6ae9bb
Adhil Athif M <adhilathif46@gmail.com>
2026-07-25 23:47:41 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.13%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        
+        int prev2 = 0; // dp[i-2]
+        int prev1 = nums[0]; // dp[i-1]
+        
+        for (int i = 1; i < nums.length; i++) {
+            int current = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
