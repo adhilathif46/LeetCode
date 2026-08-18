9a0ec8471e516dd91a727e9d5d058e4c6ab00d44
Adhil Athif M <adhilathif46@gmail.com>
2026-08-18 10:47:49 +0530

    Time: 5 ms (98.88%), Space: 43.7 MB (88.27%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int presum = 0;
+        int res = 0;
+        for (int i = satisfaction.length - 1; i >= 0; i--) {
+            presum += satisfaction[i];
+            if (presum < 0) {
+                break;
+            }
+            res = res + presum;
+        }
+        return res;
+    }
+}
