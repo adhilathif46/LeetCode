745485df1f62b14088fce41eaab3fff403aba922
Adhil Athif M <adhilathif46@gmail.com>
2026-09-03 11:34:41 +0530

    Time: 0 ms (100%), Space: 43 MB (8.82%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+    int res = 0, cnt = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s.charAt(i) == 'L') {
+            cnt++;
+        } else {
+            cnt--;
+        }
+        if (cnt == 1-1) {
+            res++;
+        }
+    }
+    return res;
+}
+}
